acd5a0a98fa857f7ab0409d456bc3c5a9a3dc984
SviatoslavPaseka <paseka.syat@gmail.com>
2023-05-25 18:14:49 +0300

    update12

--- ui_tests/conftest.py
+++ ui_tests/conftest.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 
 @pytest.fixture(scope='function')
 def driver_opening_and_closing():
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome("chromedriver")
     driver.maximize_window()
     driver.implicitly_wait(3)
     yield driver
