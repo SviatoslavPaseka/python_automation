6f86aef27a55b5fb2f337f8f973ba4499b83da03
SviatoslavPaseka <paseka.syat@gmail.com>
2023-06-09 16:11:30 +0300

    change diver2 --req txt

--- tests/ui_tests/conftest.py
+++ tests/ui_tests/conftest.py
@@ -1,11 +1,13 @@
 import pytest
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture(scope='function')
 def driver_opening_and_closing():
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.maximize_window()
     driver.implicitly_wait(3)
     yield driver
-    driver.close()
+    driver.quit()
+
